5adaf9930318c33ecc802d95102f81acadce608b
chloecombi <129711958+chloecombi@users.noreply.github.com>
2024-03-22 10:24:49 +0800

    Delete src directory

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the correct text', () => {
-  render(<App />);
-  const textElement = screen.getByText(/your text here/i);
-  expect(textElement).toBeInTheDocument();
-});
-
